2a9384d6ec25856f51573711874296406a6ffa28
marqos12 <marqos111222@gmail.com>
2019-05-10 11:24:20 +0200

    #1 dodanie lewego menu

--- ReruitmentApp/src/app/app.module.ts
+++ ReruitmentApp/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { SideMenuComponent } from './home/side-menu/side-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    SideMenuComponent
   ],
   imports: [
     BrowserModule,
